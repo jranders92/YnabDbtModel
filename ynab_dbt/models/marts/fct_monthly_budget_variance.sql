9a58026566179774a30d1bafe8df5e7f40ae51e3
ll_admin <josephranders@gmail.com>
2026-08-21 14:46:03 -0400

    pointed model from dim categories to snp ynab categories

--- ynab_dbt/models/marts/fct_monthly_budget_variance.sql
+++ ynab_dbt/models/marts/fct_monthly_budget_variance.sql
@@ -1,6 +1,13 @@
-with monthly_budget as (
-    select * from {{ ref('dim_categories') }}
-),
+with monthly_budget as (    
+    select
+        category_id,
+        budget_month,
+        budgeted_amount
+    from {{ ref('snp_ynab_categories') }}
+    qualify row_number() over (
+        partition by category_id, budget_month
+        order by dbt_valid_from desc
+    ) = 1),
 
 monthly_actuals as (
     select
@@ -12,8 +19,8 @@ monthly_actuals as (
 )
 
 select
-    md5(concat(coalesce(b.category_id, ''), '-', cast(a.budget_month as string))) as budget_variance_pk,
-    a.budget_month,
+    md5(concat(coalesce(b.category_id, ''), '-', cast(b.budget_month as string))) as budget_variance_pk,
+    b.budget_month,
     b.category_id,
     
     -- Measures
@@ -31,3 +38,4 @@ select
 from monthly_budget b
 left join monthly_actuals a 
     on b.category_id = a.category_id
+    and b.budget_month = a.budget_month
