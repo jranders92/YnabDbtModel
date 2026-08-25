b5abad448dcde11d7c5680bd231b1410f8d3b0a3
jranders92 <jranders92@users.noreply.github.com>
2026-08-25 14:54:49 +0000

    deploy: babcaacd7e84d8337092171426abb313db54e344

--- compiled/ynab_dbt/models/marts/fct_monthly_gas_inflation.sql
+++ compiled/ynab_dbt/models/marts/fct_monthly_gas_inflation.sql
@@ -1,51 +1,4 @@
-with  __dbt__cte__stg_fred_observations as (
-with raw_data as (
-    select
-        series_id,
-        payload,
-        extracted_at
-    from personal_finance.raw.raw_fred_payloads
-    where endpoint_name = 'series_observations'
-    -- Quality filter: grab the most recent extraction for each series_id
-    qualify row_number() over (
-        partition by series_id 
-        order by extracted_at desc
-    ) = 1
-),
-
-flattened_observations as (
-    select
-        raw_data.series_id,
-        obs_node.value:date::date as observation_date,
-        obs_node.value:value::string as raw_value,
-        obs_node.value:realtime_start::date as realtime_start_date,
-        obs_node.value:realtime_end::date as realtime_end_date
-    from raw_data,
-    lateral flatten(input => payload:observations) as obs_node
-)
-
-select
-    -- Primary Surrogate Key (combination of series_id and date)
-    md5(concat(series_id, '-', cast(observation_date as string))) as observation_pk,
-    
-    -- Natural Keys & Foreign Keys
-    series_id,
-    observation_date,
-
-    -- Data Cleaning: FRED returns '.' when value is missing (e.g. holidays / weekends for daily stock prices)
-    case 
-        when raw_value = '.' then null 
-        else raw_value::number(18, 4)
-    end as observation_value,
-
-    -- Metadata Dates
-    realtime_start_date,
-    realtime_end_date
-
-from flattened_observations
--- Filter out future placeholder dates if returned by API
-where observation_date <= current_date()
-), fred_seed as (
+with fred_seed as (
     select * from PERSONAL_FINANCE.analytics_prod.seed_fred_series_mapping
     where macro_category = 'Transportation'
 ),
@@ -71,7 +24,7 @@ fred_gas_prices as (
         s.series_name,
         s.units,
         avg(o.observation_value)                as avg_gas_price_per_gallon
-    from __dbt__cte__stg_fred_observations o
+    from PERSONAL_FINANCE.staging.stg_fred_observations o
     inner join fred_seed s
         on o.series_id = s.series_id
     group by 1, 2, 3, 4
--- run/ynab_dbt/models/staging/raw/ynab/_stg_raw_ynab.yml/unique_stg_ynab_transactions_transaction_id.sql
+++ run/ynab_dbt/models/staging/raw/ynab/_stg_raw_ynab.yml/unique_stg_ynab_transactions_transaction_id.sql
@@ -11,32 +11,11 @@
     
     
 
-with __dbt__cte__stg_ynab_transactions as (
-with raw_data as (
-    select payload
-    from personal_finance.raw.raw_ynab_payloads
-    where endpoint_name = 'transactions'
-    order by extracted_at desc
-    limit 1
-)
-
 select
-    value:id::string as transaction_id,
-    value:date::date as transaction_date,
-    value:amount::number / 1000.0 as amount, -- Milliunits conversion
-    value:payee_name::string as payee_name,
-    value:category_id::string as category_id,
-    value:account_id::string as account_id,
-    value:cleared::string as cleared_status,
-    value:approved::string as approved_status,
-    value:deleted::boolean as is_deleted
-from raw_data,
-lateral flatten(input => payload:data:transactions)
-) select
     transaction_id as unique_field,
     count(*) as n_records
 
-from __dbt__cte__stg_ynab_transactions
+from PERSONAL_FINANCE.staging.stg_ynab_transactions
 where transaction_id is not null
 group by transaction_id
 having count(*) > 1
